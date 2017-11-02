fdcf6b27e64efc1ddba32ec8da4fd57fdb58ef1e
mancecob <funkymonk39@gmail.com>
2017-11-02 18:14:58 +0100

    fix32325D

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 (function(){
 
-	navigator.getMedia = navigator.getUserMedia || 
-						 navigator.webkitGetUserMedia ||
-						 navigator.mozGetUserMedia ||
-						 navigator.msGetUserMedia;
 	
+
 	var video = document.getElementById('video'),
 		vendorUrl = window.URL || window.webkitURL;
 
 	
+
+	navigator.getMedia = navigator.getUserMedia || 
+						 navigator.webkitGetUserMedia ||
+						 navigator.mozGetUserMedia ||
+						 navigator.msGetUserMedia;
 
 	//Capture Video
 	navigator.getMedia({
 		video:true,
 		audio:false
 	},function(stream){
+		console.log(stream);
 		video.src = vendorUrl.createObjectURL(stream);
 		video.play();
 	},function(error){
