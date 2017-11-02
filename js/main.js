3824c3042c1d5bf6069aa3138f2e232c38527d46
mancecob <funkymonk39@gmail.com>
2017-11-02 16:50:22 +0100

    video chat test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+
+//Google login
 function onSignIn(googleUser){
 	var profile = googleUser.getBasicProfile();
 	$(".g-signin2").css("display","none");
@@ -15,3 +18,47 @@ function signOut(){
 		$(".data").css("display", "none");
 	});
 }
+
+
+//Simple Video call
+navigator.webkitGetUserMedia({video:true, audio:true}, function(stream){
+	//Simple Chat
+	var Peer = require ('simple-peer')
+	var peer = new Peer({
+		initiator: location.hash === '#init',
+		trickle: false,
+		stream: stream
+	})
+
+
+	peer.on('signal', function(data){
+		document.getElementById('yourId').value = JSON.stringify(data)
+	})
+
+	document.getElementById('connect').addEventListener('click', function(){
+		var otherId = JSON.parse(document.getElementById('otherId').value)
+		peer.signal(otherId)
+	})
+
+	document.getElementById('send').addEventListener('click', function(){
+		var yourMessage = document.getElementById('yourMessage').value
+		peer.send(yourMessage)
+	})
+
+	peer.on('data', function(data){
+		document.getElementById('messages').textContent += data + '\n'
+	})
+
+	peer.on('stream', function(stream){
+		var video = document.createElement('video')
+		document.body.appendChild(video)
+
+		video.src = window.URL.createObjectURL(stream)
+		video.play()
+	})
+
+}, function(err){
+	console.error(err);
+}
+
+
